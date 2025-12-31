c3ba33f0f38b89fbe7219f530090e2e5d07121a8
Mason Pham <mason.pham16@gmail.com>
2025-12-30 23:54:07 -0500

    changed palindrome code to reflect on what my personal solution is

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,14 +1,19 @@
 public class PalindromeNumber {
 
-    public static boolean isPalindrome(int x) {
-        if (x < 0) return false;
-        if (x % 10 == 0 && x != 0) return false;
-
-        int rev = 0;
-        while (x > rev) {
-            rev = rev * 10 + (x % 10);
-            x /= 10;
-        }
-        return x == rev || x == rev / 10;
-    }
+	public static boolean isPalindrome(int x) {
+		if(x < 0){
+			return false;
+		}
+		int reversed = 0;
+		int normal = x;
+		while(x != 0){
+			reversed = reversed*10 + x%10;
+			x = x/10;
+		}
+		if(reversed == normal){
+			return true;
+		} else{
+			return false;
+		}
+	}
 }
